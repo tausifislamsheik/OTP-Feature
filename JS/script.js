5c0e3c41ffffe0c856481a3cdd8e8bbc5658cee4
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-07-22 16:26:33 +0600

    OTP validation added with validate msg

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+let generatedOtp;
+
 const tackleOtpBoxes = () =>{
     const boxes = document.querySelector('#otp-box-list');
     boxes.addEventListener('input', (e) =>{
@@ -13,20 +15,39 @@ const tackleOtpBoxes = () =>{
         if(nextElement){
             nextElement.focus();
         }
+        validateOtp();
     });
 };
 
 
 const generateOtp = () =>{
-    const generatedOtp = Math.floor(1000 + Math.random() * 9000);
-    const otpEle = document.querySelector('#generated-otp');
-    otpEle.innerHTML = `
+    generatedOtp = Math.floor(1000 + Math.random() * 9000);
+    const otpElem = document.querySelector('#generated-otp');
+    otpElem.innerHTML = `
           <p>Your OTP : <b class='border p-1 rounded-xl'>${generatedOtp}</b></p>
     `
 
 }
 
-
+const validateOtp = () =>{
+    let typedNumber = '';
+    const otpBoxList = document.querySelector('#otp-box-list');
+    [...otpBoxList.children].forEach((elem) =>{
+        typedNumber = typedNumber + elem.value;
+        
+    });
+    
+    const result = (generatedOtp === parseInt(typedNumber, 10));
+    const otpValidateMsg = document.querySelector('#otp-validate-msg');
+    if(result){
+        otpValidateMsg.innerText = `OTP has been validate successfully`;
+        otpValidateMsg.style.backgroundColor = 'green';
+        
+    }else{
+        otpValidateMsg.innerText = `OTP is Invalid`;
+        otpValidateMsg.style.backgroundColor = 'red';
+    }
+}
 
 const otpContainer = () =>{
   tackleOtpBoxes();
