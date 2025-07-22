0855c6952134dd7ce053cfa5c3dc6a3a55531561
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-07-22 15:52:25 +0600

    Auto OTP generated logic added with loading feature

--- JS/script.js
+++ JS/script.js
@@ -17,8 +17,20 @@ const tackleOtpBoxes = () =>{
 };
 
 
+const generateOtp = () =>{
+    const generatedOtp = Math.floor(1000 + Math.random() * 9000);
+    const otpEle = document.querySelector('#generated-otp');
+    otpEle.innerHTML = `
+          <p>Your OTP : <b class='border p-1 rounded-xl'>${generatedOtp}</b></p>
+    `
+
+}
+
+
+
 const otpContainer = () =>{
   tackleOtpBoxes();
+  setTimeout(generateOtp, 2000);
 };
 
 
